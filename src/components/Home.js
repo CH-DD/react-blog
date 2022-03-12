0f901394b3ec2ae706f75d0323c1a0eacba7ac00
CH-DD <68435229+CH-DD@users.noreply.github.com>
2022-03-12 21:37:20 +0000

    Tutorial #8 - Using State (useState hook) complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,30 @@
+import { useState } from "react";
+
 const Home = () => {
 
+    // useState hook used for 'reactive' values that may change at some point
+    // how: use array destructuring to grab 2 values that the hook returns to us: 1. initial value (eg. name) 2. function used to change the value (eg. setName)
+    // value can be any type. ie. string, number, array, object, boolean
+
+    const [name, setName] = useState("Mario"); 
+    const [age, setAge] = useState(25);
+        
+
+    // useState triggers React to re-render the component and update the values on click
     const handleClick = (eventObject) => {
-        console.log("Hi there", eventObject);  // eventObject automatically available when an event occurs
+        setName("Luigi");  
+        setAge(30);
     }
 
-    const handleClickAgain = (name, eventObject) => {
-        console.log("Hi " + name, eventObject.target);
-    }
-
-    return ( 
+    return (
         <main className="home">
             <section className="content">
+
                 <h2>Homepage</h2>
-                <p>
-                    <button onClick={ handleClick }>Click me</button>
-                </p>
+                <p>{ name } is { age } years old.</p>
 
                 <p>
-                    <button onClick={(eventObject) => handleClickAgain("Bill", eventObject)}>
-                        Click me again
-                    </button>
-
+                    <button onClick={handleClick}>Click me</button>
                 </p>
                 
             </section>
