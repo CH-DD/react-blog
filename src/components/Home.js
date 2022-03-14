2fd7b72d4770f739571eaa0c8c8dcf370832afaa
CH-DD <68435229+CH-DD@users.noreply.github.com>
2022-03-14 11:06:35 +0000

    Tutorial #15 - useEffect Dependencies complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,15 @@ import ArticlesList from "./ArticlesList";
 
 const Home = () => {
 
-    // State: Stores data. Initial 'reactive' list of articles
+    // State: Stores data. 
+    // Initial 'reactive' list of articles
     const [articles, setArticles] = useState([
         { id: 1, author: "Mario", title: "My new website", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
         { id: 2, author: "Yoshi", title: "Welcome party!", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
         { id: 3, author: "Mario", title: "Web development top tips", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
     ]);
+    // Name value to test useEffect 'dependency array'
+    const [name, setName] = useState("Mario");
 
     // Functionality: Delete an article
     // - 1. Create new list of articles that DO NOT have the same id value as article being deleted. Initial array not mutated.
@@ -22,16 +25,19 @@ const Home = () => {
     // useEffect: runs a function on every render (passed in as an argument).  ie. when component first loads, and whenever 'state' data changes
     // Common uses - to fetch data, or communicate with auth. service (aka 'side effects')
     // Be careful changing state in here - can create infinite loop
+    // To define when exactly the function should be rendered (ie. not every render), include optional 'dependency array' as a second argument. An empty array means first render only.
     useEffect(() => {
-        // test values
         console.log("use effect ran");
-        console.log(articles);    
-    });
+        console.log(name);
+    }, [name]);  // only run when 'name' changes
 
 
     return (
         <main className="home">
             <div className="content">
+
+                <button onClick={ () => setName("Luigi") }>Change Name</button>
+                <p>{ name }</p>
 
                 {/* List all articles
                 - Pass props into 'ArticlesList' child component: articles array, title, handleDelete function */}
