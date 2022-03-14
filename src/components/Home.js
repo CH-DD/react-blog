76a2a1e26e6c2720faead29dd70a3aa98479136c
CH-DD <68435229+CH-DD@users.noreply.github.com>
2022-03-14 18:04:32 +0000

    Tutorial #19 - Handling Fetch Errors complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,16 @@ import ArticlesList from "./ArticlesList";
 
 const Home = () => {
 
-    // State: 
-    // Initial 'reactive' list of articles. Data fetched from fake local API: using 'json-server' package and data/dummy-db.json
+    // State values: 
+    // Initial list of articles. Data fetched from fake local API using 'json-server' package and data/dummy-db.json
     const [articles, setArticles] = useState(null); // null will be updated when data is fetched 
 
     // Loading message whilst fetching data
     const [isLoading, setIsLoading] = useState(true);  // initial value: true
+
+    // Error handling
+    const [error, setError] = useState(null);
+
 
     // useEffect: fetch articles data from API
     // function only fired once - on initial render (due to empty dependency array)
@@ -17,17 +21,25 @@ const Home = () => {
 
         // temporarily wrap fetch in 'setTimeout' to simulate a real web server
         setTimeout(() => {  
-
             fetch("http://localhost:8000/articles")
-            .then(res => {              // get response object 
-                return res.json();      // parse json into js object & return a promise
-            }) 
-            .then(data => {             // get the returned data: as array of objects 
-                console.log("Fetched array -->", data);
-                setArticles(data);      // update state with the fetched array
-                setIsLoading(false);    // update loading message state
-            })
 
+                .then(response => {         // get response object 
+                    if(!response.ok) {      // catch data request errors
+                        return setError("We could not find the thing you wanted.");
+                    }
+                    return response.json(); // parse json --> object & return a promise
+                }) 
+                .then(data => {             // get returned data: array of objects 
+                    setArticles(data);      // update state with the fetched array
+                    setIsLoading(false);    // update loading message state
+                })
+                .catch((err) => {           // catch network connection errors
+                    // set error message
+                    setError("We seem to be experiencing connection issues.");
+                    // remove loading message: no longer required
+                    setIsLoading(false);
+                })
+                
         }, 1000); // time delay in ms
 
     }, []);   
@@ -36,15 +48,29 @@ const Home = () => {
     return (
         <main className="home">
             <div className="content">
-                {/* Loading message */}
-                {isLoading && 
-                    <div className="loading">
-                        <span className="material-icons">sync</span>
-                        <span className="text">Loading...</span>
+
+                {/* Error message (if any exist) */}
+                {error && 
+                    <div id="error">
+                        <h2>
+                            <span className="material-icons">error_outline</span>
+                            <span className="text">Oops! { error }</span>
+                        </h2>
+                        <p>Maybe try again?</p>
                     </div>
                 }
 
-                {/* List all articles
+                {/* Loading message */}
+                {isLoading && 
+                    <div id="loading">
+                        <h2>
+                            <span className="material-icons">sync</span>
+                            <span className="text">Loading...</span>
+                        </h2>
+                    </div>
+                }
+
+                {/* List articles
                 - 'Conditional template': code to right of the logical AND (&&) is only output if both 'articles' and 'ArticlesList' evaluate to true. ie. 'articles' data has been fetched and the initial 'null' value is overwritten.
                 - Pass props into 'ArticlesList' child component: articles array, title */}
                 {articles && <ArticlesList articles={articles} title="All Articles" />}
